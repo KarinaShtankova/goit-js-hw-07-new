8395e00c4ddd74f1a7f75501fde7b99f2ecf753a
KarinaShtankova <shtankovads@gmail.com>
2023-11-17 23:45:27 +0200

    adds task -01

---
+++ js/task-01.js
@@ -0,0 +1,14 @@
+const items = document.querySelector("ul#categories");
+const categoriesQuantity = items.children.length;
+console.log(`Number of categories: ${categoriesQuantity}`);
+
+for (let index = 0; index < categoriesQuantity; index++) {
+	const category = items.children[index];
+	const categoryTitle = category.firstElementChild.textContent;
+
+	const elements = category.lastElementChild.children.length;
+
+	console.log(`Category: ${categoryTitle}`);
+
+	console.log(`Elements: ${elements}`);
+}
